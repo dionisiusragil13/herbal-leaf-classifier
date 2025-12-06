8b362c74bc3c1a85627e825b4fcd258e8674dde2
dionisiusragil13 <dionisiusragil13@gmail.com>
2025-12-06 12:50:19 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <button className="inline-block cursor-pointer uppercase text-sm"> tombol</button>
+  );
+}
+
+export default App;
