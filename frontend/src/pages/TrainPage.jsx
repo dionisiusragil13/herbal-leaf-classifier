d56a58936231fd891644fa7ee52cdb4094e20100
dionisiusragil13 <dionisiusragil13@gmail.com>
2025-12-10 20:31:31 +0700

    implementing training progress ui and connected it to the backend

--- frontend/src/pages/TrainPage.jsx
+++ frontend/src/pages/TrainPage.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import InputParameter from "../components/InputParameter";
 import TextWithHighlight from "../components/HighLightedText";
+import ProgressBar from "../components/ProgressBar";
+import useTrainStore from "../store/useTrainStore";
 
 function TrainPage() {
+  const { isTraining } = useTrainStore();
   return (
     <div className="min-h-scren flex flex-col items-center justify-center mt-12">
       <img
         src="/leaf-1.png"
-        className="absolute left-43 bottom-5 w-[300px] z-0 opacity-90"
+        className="absolute left-43 top-120 w-[300px] z-0 opacity-90"
         alt=""
       />
 
       <img
         src="/leaf-2.png"
-        className="absolute right-40 top-40 w-[320px] z-0 opacity-90"
+        className="absolute right-40 top-60 w-[320px] z-0 opacity-90"
         alt=""
       />
       <div className="">
@@ -24,11 +27,15 @@ function TrainPage() {
           />
         </h1>
       </div>
-      <div className="z-10">
+      <div className="z-10 mb-5">
         <InputParameter />
       </div>
       {/*progress training */}
-      <div>test</div>
+      {isTraining && (
+        <div className="mt-4 p-3 bg-blue-50 text-blue-700 rounded-md text-sm">
+          ⚡ Training in progress... Please don't close this page.
+        </div>
+      )}
     </div>
   );
 }
